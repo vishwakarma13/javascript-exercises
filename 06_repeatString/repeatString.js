b8d06e10d5d0a88f12bcfe2b31196bfd3bacd2fb
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-12 01:06:02 +0530

    add the required function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+    return str.repeat(rep);
 };
+
+
+let newString=repeatString("hey", 3);
+console.log(newString);
 
 // Do not edit below this line
 module.exports = repeatString;
